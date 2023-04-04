7ccb77d5c06529a8367b6ff05adbd46cf5409f58
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-04-04 06:18:10 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,56 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listImgEl = document.querySelector('.gallery')
+
+const imgCreateEl = createGaleryImg();
+
+function createGaleryImg() {
+    return galleryItems.map(({ preview, description, original }) => {
+        return  `<li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            data-source="${original}"
+                            alt="${description}"
+                        />
+                    </a>
+                </li>`
+        
+        
+    }).join("")
+}
+
+listImgEl.insertAdjacentHTML('beforeend', imgCreateEl);
+
+
+
+listImgEl.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+
+    if (!event.target.classList.contains('gallery__image')) { return; };
+    // console.log(event.target)
+
+    const dataSourseId = event.target.dataset.source
+    console.log(dataSourseId)
+
+    const instance = basicLightbox.create(`
+        <div class="modal">
+            <img
+                class="gallery__image"
+                src="${dataSourseId}"
+                data-source="${dataSourseId}"
+                
+            />
+        </div>
+    `)
+
+    instance.show()
+
+    const modalDiv = document.querySelector('.modal');
+    modalDiv.addEventListener('click', (event) => { instance.close() });
+});
+
